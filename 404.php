df04e8a91df79a00f0a00d71db6fa3431f35a77b
kseniaazovtseva <ksufanka91@mail.ru>
2025-05-21 12:12:08 +0300

    intergate 404 page

--- 404.php
+++ 404.php
@@ -7,17 +7,21 @@ CHTTP::SetStatus("404 Not Found");
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
 
 $APPLICATION->SetTitle("Страница не найдена");
+?>
 
-$APPLICATION->IncludeComponent(
-    "bitrix:main.map",
-    ".default",
-    [
-        "LEVEL" => "3",
-        "COL_NUM" => "2",
-        "SHOW_DESCRIPTION" => "Y",
-        "SET_TITLE" => "N",
-        "CACHE_TIME" => "36000000",
-    ]
-);
+<div class="error-area ptb-120">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="error-content text-center">
+                    <h1>404</h1>
+                    <p>Страница, которую вы ищете, не найдена!</p>
+                    <a href="/" class="btn">На главную</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 
+<?php
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
